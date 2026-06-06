9ccd866837d08e632c071a7261336eefccde5f0a
Guilherme Lima <guilhermevictorrocha@gmail.com>
2026-06-06 17:08:22 -0300

    feat: criando entidades

---
+++ Data/Entities/LancamentoFinanceiro.cs
@@ -0,0 +1,34 @@
+﻿namespace Data.Entities
+{
+    using Data.Enums;
+
+    public class LancamentoFinanceiro
+    {
+
+        required public int Id { get; init; }
+
+        required public string Descricao { get; set; }
+
+        required public TipoLancamento Tipo { get; set; }
+
+        required public decimal ValorOriginal { get; set; }
+
+        required public decimal PercentualTaxa { get; set; }
+
+        required public decimal PercentualDesconto { get; set; }
+
+        required public decimal ValorCalculado { get; set; }
+
+        required public DateTime DataLancamento { get; set; }
+
+        required public DateTime DataCriacao { get; set; }
+
+        public DateTime? DataPagamento { get; set; }
+
+        public DateTime? DataCancelamento { get; set; }
+
+        required public string Competencia { get; set; }
+
+        required public StatusLancamento Status { get; set; }
+    }
+}
---
+++ Data/Enums/TipoLancamento.cs
@@ -0,0 +1,8 @@
+﻿namespace Data.Enums
+{
+    public enum TipoLancamento
+    {
+        Credito,
+        Debito,
+    }
+}
