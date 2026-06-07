97100bb6c9d353335037bd848c60aad5c8d0077f
Guilherme Lima <guilhermevictorrocha@gmail.com>
2026-06-07 08:50:39 -0300

    feat: implementando lógica abstrata do RepositoryBase

--- Data/Repositories/LancamentoFinanceiroRepository.cs
+++ Data/Repositories/LancamentoFinanceiroRepository.cs
@@ -1,8 +1,18 @@
 ﻿namespace Data.Repositories
 {
     using Data.Entities;
+    using Microsoft.Data.SqlClient;
 
-    public class LancamentoFinanceiroRepository : RepositoryBase<LancamentoFinanceiro, int>
+    public class LancamentoFinanceiroRepository : RepositoryBase<string, int>
     {
+        public string Get()
+        {
+            return this.Get("SELECT @@SERVICENAME as service_name") ?? "Nenhum resultado encontrado";
+        }
+
+        internal override string EntityMapper(SqlDataReader reader)
+        {
+            return $"Service Name: {reader.GetString(0)}";
+        }
     }
 }
--- Data/Repositories/RepositoryBase.cs
+++ Data/Repositories/RepositoryBase.cs
@@ -1,23 +1,56 @@
 ﻿namespace Data.Repositories
 {
-    // TODO implementar conexão com banco
-    public class RepositoryBase<TEntity, TId>
+    using Microsoft.Data.SqlClient;
+
+    public abstract class RepositoryBase<TEntity, TId>
     {
+
+        private readonly string connectionString = "Server=localhost;Database=controle_financeiro;User Id=sa;Password=admin;TrustServerCertificate=True;";
+
 
         public List<TEntity> List(string sql)
         {
-            return
-                [];
+            return this.ExecuteSQL(sql, this.EntityListMapper) ?? [];
         }
 
-        public TEntity? Get(string sql)
+        public virtual TEntity? Get(string sql)
         {
-            return default;
+            return this.ExecuteSQL(sql, this.EntityMapper);
         }
 
         public TEntity? Update(string sql)
         {
             return default;
+        }
+
+        internal abstract TEntity EntityMapper(SqlDataReader reader);
+
+        internal List<TEntity> EntityListMapper(SqlDataReader reader)
+        {
+            List<TEntity> result = [];
+
+            while (reader.Read())
+            {
+                result.Add(this.EntityMapper(reader));
+            }
+
+            return result;
+        }
+
+        private TConnectionReturn? ExecuteSQL<TConnectionReturn>(string sql, Func<SqlDataReader, TConnectionReturn> dataMapper)
+        {
+            using SqlConnection connection = new (this.connectionString);
+            connection.Open();
+
+            using SqlCommand command = new (sql, connection);
+            using SqlDataReader reader = command.ExecuteReader();
+
+            if (!reader.Read())
+            {
+                return default;
+            }
+
+            return dataMapper(reader);
         }
 
     }
